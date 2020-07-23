55a4362f819ffddb529827240382da4a1b3358bc
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-07-23 20:55:06 +0900

    平均点閲覧機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,21 @@ class UsersController < ApplicationController
   def show
     @user = current_user
 
+    # 得点のグラフ表示
     @answer_sheets = AnswerSheet.where(user_id: current_user.id).order(:id)
     @point_charts = []
     @answer_sheets.each do |as|
       point_chart = [as.exam.title, as.point]
       @point_charts << point_chart
+    end
+
+    # 平均点のグラフ表示
+    @average = AnswerSheet.group(:exam_id).average(:point).sort.to_h
+    @average_charts = []
+    @average.values.each_with_index do |value, i|
+      exam = Exam.find(i + 1)
+      average_chart = [exam.title, value]
+      @average_charts << average_chart
     end
   end
 
--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,3 +1,9 @@
 Chartkick.options = {
-  colors: ["#4242ff", "#ffd1a3"]
+  max: 5,
+  curve: false,
+  suffix: "点",
+  round: 2,
+  messages: {empty: "No data"},
+  dataset: { borderWidth: 2 },
+  colors: ["#00ACC1", "#FFA000"]
 }
