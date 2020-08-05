0f08d6e78b907a57b4ee86a2275e2d6975aa863e
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-08-05 19:03:02 +0900

    ユーザ関連機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,22 +19,4 @@ class UsersController < ApplicationController
       @average_charts << average_chart
     end
   end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to user_path
-    else
-      render :edit
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email)
-  end
 end
