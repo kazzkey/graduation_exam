bd1a525e56dd5790fb9095563d013ab8c63e8f18
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-08-05 09:58:16 +0900

    試験締切通知のhome画面表示実装と通知表示の細かな修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @exams = Exam.where(release: true).where("deadline > ?", Date.current)
+    # 締切の過ぎた問題をはじく
+    @exams = Exam.where(release: true).where("deadline >= ?", Date.current)
+    # 締切3日以内の試験を取り出す
+    @exams_on_deadline = @exams.where("deadline <= ?", Date.current + 3)
   end
 end
