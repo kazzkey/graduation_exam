4e0df70051f9c29d1096c2e399c3cf68c8b74318
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-08-13 13:49:56 +0900

    アップロード画像のリサイズを無効に

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [940, 705, "Center"]
+  # process resize_to_fill: [940, 705, "Center"]
   #
   # def scale(width, height)
   #   # do something
