d0dd3ff30952b728255da428a6dd9472fe957e03
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-09-13 23:02:23 +0900

    テスト修正

--- spec/system/exam_spec.rb
+++ spec/system/exam_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe '試験作成機能', type: :system do
         click_on '新規作成'
         fill_in '試験名', with: 'test_title'
         find('.subject').select('Japanese')
-        fill_in '締切', with: '002020-12-31'
+        fill_in '締切', with: '2020-12-31'
         check '公開'
         find('.image_form_1', visible: false, match: :first).set("#{Rails.root}/spec/factories/test.jpg")
         for n in 0..4 do
@@ -110,7 +110,7 @@ RSpec.describe '試験作成機能', type: :system do
         find('.fa-edit').click
         sleep 0.5
         check '公開'
-        fill_in '締切', with: '002020-12-31'
+        fill_in '締切', with: '2020-12-31'
         click_on '更新'
         sleep 0.5
         expect(page).to have_content '公開'
@@ -174,8 +174,7 @@ RSpec.describe '試験作成機能', type: :system do
         click_on '復習問題'
         fill_in '試験名', with: 'REtest_title'
         find('.subject').select('Japanese')
-        # fill_in '締切', with: '002020-12-31'
-        select_date('2020,12,31', from: '締切')
+        fill_in '締切', with: '002020-12-31'
         check '公開'
         click_on '問題選択'
         for n in 0..4 do
